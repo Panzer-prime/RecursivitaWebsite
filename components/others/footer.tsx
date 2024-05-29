70c78013d055b3c4fbcb4756bebea505f7ca5ad8
Panzer <119689044+Panzer-prime@users.noreply.github.com>
2024-05-29 18:26:54 +0300

    Update footer.tsx

--- components/others/footer.tsx
+++ components/others/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
           <div className="container mx-auto flex justify-between items-center">
             <p className="text-sm">&copy; {new Date().getFullYear()} Suhan Gabriel. All rights reserved.</p>
             <div>
-              <p className="text-sm">Follow me on GitHub:</p>
+              <p className="text-sm">Code in github:</p>
               <a href="https://github.com/Panzer-prime" target="_blank" rel="noopener noreferrer" className="text-goldYellow hover:text-blue-500 transition duration-300">Panzer</a>
             </div>
           </div>
